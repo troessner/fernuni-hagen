fad9421d0e8c35a88264286905d3de2352617d64
Timo Rößner <timo.roessner@googlemail.com>
2019-04-10 01:22:40 +0200

    Add A 2.4.

---
+++ Einfuehrung in die imperative Programmierung/Aufgabe_2_4.pas
@@ -0,0 +1,17 @@
+program WasPassiert(input, output);
+var
+  a, b, c: Integer;
+
+begin
+  b := 0;
+  c := 1;
+  readln(a);
+  while a > 0 do
+  begin
+    b := b+c*(a mod 2);
+    a := a div 2;
+    c := c*10;
+  end;
+  writeln(b)
+end.
+
