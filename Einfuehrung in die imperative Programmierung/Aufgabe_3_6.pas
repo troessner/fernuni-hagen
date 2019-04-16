597788c4b6895e2eea482712e7a2cbf0ee88542b
Timo Rößner <timo.roessner@googlemail.com>
2019-04-16 23:38:48 +0200

    Add A 3.5 and A 3.6.

---
+++ Einfuehrung in die imperative Programmierung/Aufgabe_3_6.pas
@@ -0,0 +1,55 @@
+{
+* Ergebnis:
+* 1.) "Die Parameterübergaben beider Prozeduren erfolgen als var-Parameter." -> 1, -3, 4
+* 2.) "Die Parameterübergaben beider Prozeduren erfolgen als Wertparameter." -> 1, 5, 6
+* 3.) "Die Parameterübergabe erfolgt bei der Prozedur globalProc als var-Parameter
+*     und bei der Prozedur lokalProc als Wertparameter." -> 1, -3, 4
+* 4.) "Die Parameterübergabe erfolgt bei der Prozedur globalProc als
+*     Wertparameter und bei der Prozedur lokalProc als var-Parameter." -> 1, 5, 6
+* 5.) "	Die Ausgabe des Programms ist unabhängig von der Art der Parameterübergabe."
+*     -> Offensichtlich falsch, siehe Ergebnisse oben.
+* }
+
+program Parameter1 (output);
+{ Dieses Programm hat keine sinnvolle Semantik, zeigt schlech-
+  ten Programmierstil und dient nur zum Ueben der Parameterueber-
+  gabearten. }
+
+  var 
+  a,
+  b,
+  c : integer;
+
+  procedure globalProc ({call_by_1} d, e, f: integer);
+
+    var 
+    a : integer; 
+
+    procedure lokalProc ({call_by_2} var g, h, i: integer);
+
+      var 
+      b : integer;
+
+    begin
+      g := h + i;
+      b := c;
+      a := 2 * b;
+      i := a + b
+    end; { lokalProc }
+
+  begin
+    a := 4;
+    b := 5;
+    c := 6;
+    d := e - f;
+    lokalProc (a, d, e);
+    e := f - d
+  end; { globalProc }
+
+begin
+  a := 1;
+  b := 2;
+  c := 3;
+  globalProc (c, b, a);
+  writeln (a, ' ', b, ' ', c)
+end. { Parameter1 }
